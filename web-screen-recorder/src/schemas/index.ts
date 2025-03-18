b9c87f6058b5d7a24599685e243701a6a8d09553
devcomfort <im@devcomfort.me>
2025-03-18 11:34:29 +0000

    fix : 타입 내보내기 수정 (export -> export type)

--- web-screen-recorder/src/schemas/index.ts
+++ web-screen-recorder/src/schemas/index.ts
@@ -1,11 +1,9 @@
-export { MediaType, MediaType_ } from "./media-type";
+export { MediaType_ } from "./media-type";
+export type { MediaType } from "./media-type";
 export { MediaRecorderOptions_ } from "./media-recorder-options";
-export {
-	EnhancedMediaRecorderOptions,
-	EnhancedMediaRecorderOptions_,
-} from "./enhanced-media-recorder-options";
-export {
-	MediaFormatSupport,
-	MediaFormatSupport_,
-} from "./media-format-support";
-export { MediaFormat, MediaFormat_ } from "./media-format";
+export { EnhancedMediaRecorderOptions_ } from "./enhanced-media-recorder-options";
+export type { EnhancedMediaRecorderOptions } from "./enhanced-media-recorder-options";
+export { MediaFormatSupport_ } from "./media-format-support";
+export type { MediaFormatSupport } from "./media-format-support";
+export { MediaFormat_ } from "./media-format";
+export type { MediaFormat } from "./media-format";
